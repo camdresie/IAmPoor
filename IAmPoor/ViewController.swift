1a02a91eeb1e10bbc2bd50d268e2c721694b0f97
Cam <cameron.riemensnider@gmail.com>
2021-10-31 15:58:04 -0700

    Adding all files

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Cam Riemensnider on 10/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
